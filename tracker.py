aa83c5c0a5fb7da95bc3549dbe0aa417ccbef1fb
Feifan <feifan.he@mail.utoronto.ca>
2023-03-25 18:58:32 -0400

    add summerize by date, month, year and testcases

--- tracker.py
+++ tracker.py
@@ -1,27 +1,27 @@
 import sys
 from transaction import Transaction
 
+HELP = '''Options:
+     0. quit
+     1. show categories
+     2. add category
+     3. modify category
+     4. show transactions
+     5. add transaction
+     6. delete transaction
+     7. summarize transactions by date
+     8. summarize transactions by month
+     9. summarize transactions by year
+     10. summarize transactions by category
+     11. print this menu
+'''
 
 
 def print_usage():
     '''Print an explanation of how to use this command.'''
-    print('''Options:
-             0. quit
-             1. show categories
-             2. add category
-             3. modify category
-             4. show transactions
-             5. add transaction
-             6. delete transaction
-             7. summarize transactions by date
-             8. summarize transactions by month
-             9. summarize transactions by year
-             10. summarize transactions by category
-             11. print this menu
-''')
+    print(HELP)
 
 
-    
 def process_args(tracker, arglist):
     '''Examine args and make appropriate calls to Transaction class.'''
     if arglist == []:
@@ -36,31 +36,37 @@ def process_args(tracker, arglist):
         tracker.modify_category(arglist[1], arglist[2])
     elif arglist[0] == "4":
         for transaction in tracker.show_transactions():
-             print(transaction)
+            print(transaction)
     elif arglist[0] == "5":
         tracker.add_transaction(arglist[1], arglist[2], arglist[3], arglist[4])
     elif arglist[0] == "6":
         tracker.delete_transaction(arglist[1])
-    
-        '''Don't forget to add other features from 7-10'''
-    
-    
-    
-    
+    elif arglist[0] == "7":
+        tracker.summarize_transactions_by_date()
+    elif arglist[0] == "8":
+        tracker.summarize_transactions_by_month()
+    elif arglist[0] == "9":
+        tracker.summarize_transactions_by_year()
+
+        '''Don't forget to add other features from 10'''
+
+
+
+
     elif arglist[0] == "11":
         print_usage()
     else:
         print(arglist, "is not implemented")
         print_usage()
 
-        
+
 def main():
     '''Read the command args and process them.'''
     tracker = Transaction()
-    
+
     if len(sys.argv) == 1:
         # They didn't pass any arguments, so prompt for them in a loop
-        #so prompt for them in a loop
+        # so prompt for them in a loop
         print_usage()
         args = []
         while args != ['0']:
@@ -72,8 +78,7 @@ def main():
         args = sys.argv[1:]
         process_args(tracker, args)
         print('-' * 40 + '\n' * 3)
-    
-    
+
 
 if __name__ == '__main__':
     main()
--- transaction.py
+++ transaction.py
@@ -19,63 +19,67 @@ In addition, the ORM includes a method runQuery() that connects to the database,
 import sqlite3
 import os
 
+DB_FILE_PATH = 'tracker.db'
+
+
 class Transaction:
     def __init__(self):
-        self.db_file = os.path.join(os.getenv('HOME'), 'tracker.db')
         self.create_tables()
 
     def create_tables(self):
-       # self.runQuery('''CREATE TABLE IF NOT EXISTS categories (
-       #                     id INTEGER PRIMARY KEY,
-       #                     name TEXT UNIQUE NOT NULL)''', ())
+        # self.runQuery('''CREATE TABLE IF NOT EXISTS categories (
+        #                     id INTEGER PRIMARY KEY,
+        #                     name TEXT UNIQUE NOT NULL)''', ())
 
         self.runQuery('''CREATE TABLE IF NOT EXISTS transactions (
                         id INTEGER PRIMARY KEY,
                         date TEXT NOT NULL,
                         description TEXT NOT NULL,
-                        amount REAL NOT NULL,
-                        category TEXT NOT NULL)''', ())
+                        amount REAL NOT NULL)''', ())
 
+    # features created by Yingshan Hu
+    # def show_categories(self):
+    #   return self.runQuery("SELECT * FROM categories", ())
 
-        
-     # features created by Yingshan Hu
-     #def show_categories(self):
-     #   return self.runQuery("SELECT * FROM categories", ())
-    
-     # features created by Yingshan Hu
-    #def add_category(self, name):
+    # features created by Yingshan Hu
+    # def add_category(self, name):
     #    self.runQuery("INSERT INTO categories(name) VALUES(?)", (name,))
-     
-     # features created by Yingshan Hu
-    #def modify_category(self, old_name, new_name):
+
+    # features created by Yingshan Hu
+    # def modify_category(self, old_name, new_name):
     #    self.runQuery("UPDATE categories SET name = ? WHERE name = ?", (new_name, old_name))
-         
+
     # features created by Bing Han
     def show_transactions(self):
         return self.runQuery("SELECT * FROM transactions", ())
 
-  # features created by Bing Han
-    def add_transaction(self, amount, category, date, description):
-        self.runQuery("INSERT INTO transactions (amount, category, date, description) VALUES (?, ?, ?, ?)",
-            (amount, category, date, description))
-
+    # features created by Bing Han
+    def add_transaction(self, date, description, amount):
+        self.runQuery("INSERT INTO transactions (date, description, amount) VALUES (?, ?, ?)",
+                      (date, description, amount))
 
     # features created by Bing Han
     def delete_transaction(self, transaction_id):
         self.runQuery("DELETE FROM transactions WHERE id = ?", (transaction_id,))
 
-    
-    
-    
-    '''Don't forget to add other features from 7-10'''
-    
-    
-    
-    
-        
-     def runQuery(self, query, params):
+    # feature created by Feifan He
+    def summarize_transactions_by_date(self):
+        return self.runQuery("SELECT date, SUM(amount) FROM transactions GROUP BY date", ())
+
+    # feature created by Feifan He
+    def summarize_transactions_by_month(self):
+        return self.runQuery("SELECT strftime('%Y-%m', date) AS month, SUM(amount) FROM transactions GROUP BY month",
+                             ())
+
+    # feature created by Feifan He
+    def summarize_transactions_by_year(self):
+        return self.runQuery("SELECT strftime('%Y', date) AS year, SUM(amount) FROM transactions GROUP BY year", ())
+
+    '''Don't forget to add other features from 10'''
+
+    def runQuery(self, query, params):
         try:
-            conn = sqlite3.connect(self.db_file)
+            conn = sqlite3.connect(DB_FILE_PATH)
             cur = conn.cursor()
             cur.execute(query, params)
             conn.commit()
