a0250da882b28c409468ffbc9fa742d89f7eacfe
ysshanhu <123339267+ysshanhu@users.noreply.github.com>
2023-03-25 17:24:38 +0800

    Added print_usage

--- tracker.py
+++ tracker.py
@@ -3,6 +3,23 @@ from transaction import Transaction
 
 
 
+def print_usage():
+    '''Print an explanation of how to use this command.'''
+    print('''Options:
+             0. quit
+             1. show categories
+             2. add category
+             3. modify category
+             4. show transactions
+             5. add transaction
+             6. delete transaction
+             7. summarize transactions by date
+             8. summarize transactions by month
+             9. summarize transactions by year
+             10. summarize transactions by category
+             11. print this menu
+''')
+
 
 
 if __name__ == '__main__':
